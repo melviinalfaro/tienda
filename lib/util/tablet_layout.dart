95b24ac14e2b48073e8dc0331c2cb7865083e793
Melvin Alfaro <98721327+melviinalfaro@users.noreply.github.com>
2023-01-13 16:06:58 -0600

    Nuevos cambios

--- lib/util/tablet_layout.dart
+++ lib/util/tablet_layout.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:tienda/components/appbar_tablet.dart';
 
-class MyTabletLayout extends StatelessWidget {
+class MyTabletLayout extends StatefulWidget {
   const MyTabletLayout({super.key});
+  @override
+  State<MyTabletLayout> createState() => _TabletState();
+}
 
+class _TabletState extends State<MyTabletLayout> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appbarTablet,
+      body: Row(
+        children: [
+          NavigationRail(
+            indicatorColor: Colors.blue,
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.search),
+                label: Text('Search'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+          Expanded(
+            child: IndexedStack(
+              index: _selectedIndex,
+              children: [
+                /*
+                HomePage(),
+                SearchPage(),
+                SettingsPage(), */
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
