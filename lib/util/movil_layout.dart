e4f669e0dcf960d251789969cf4779ddf3e05c55
Melvin Alfaro <98721327+melviinalfaro@users.noreply.github.com>
2023-01-13 13:30:09 -0600

    Actualización de colores

--- lib/util/movil_layout.dart
+++ lib/util/movil_layout.dart
@@ -21,8 +21,8 @@ class _MovilState extends State<MyMovilLayout> {
       ),
       bottomNavigationBar: NavigationBarTheme(
           data: NavigationBarThemeData(
-            backgroundColor: Theme.of(context).brightness == Brightness.dark ? const Color(0xFF252525) : Colors.white,
-            indicatorColor: Colors.indigo.shade100,
+            backgroundColor: Theme.of(context).brightness == Brightness.dark ? const Color(0xFF252525) : const Color(0xFFE9EFF7),
+            indicatorColor: Theme.of(context).brightness == Brightness.dark ? const Color(0xFF9FCAFF) : const Color(0xFFD7E3F7),
             height: 80,
             labelTextStyle: MaterialStateProperty.all(
               const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
@@ -31,30 +31,30 @@ class _MovilState extends State<MyMovilLayout> {
           child: NavigationBar(
             destinations: const [
               NavigationDestination(
-                icon: Icon(Icons.home_outlined, size: 24),
+                icon: Icon(Icons.home_outlined, size: 26),
                 selectedIcon:
-                    Icon(Icons.home_rounded, color: Colors.indigo, size: 24),
+                    Icon(Icons.home_rounded, color: Color(0xFF0A1E27), size: 26),
                 label: 'Inicio',
                 tooltip: '',
               ),
               NavigationDestination(
-                icon: Icon(Icons.smartphone_outlined, size: 24),
+                icon: Icon(Icons.smartphone_outlined, size: 26),
                 selectedIcon: Icon(Icons.smartphone_rounded,
-                    color: Colors.indigo, size: 24),
+                    color: Color(0xFF0A1E27), size: 26),
                 label: 'Catálogo',
                 tooltip: '',
               ),
               NavigationDestination(
-                icon: Icon(Icons.shopping_bag_outlined, size: 24),
+                icon: Icon(Icons.shopping_bag_outlined, size: 26),
                 selectedIcon: Icon(Icons.shopping_bag_rounded,
-                    color: Colors.indigo, size: 24),
+                    color: Color(0xFF0A1E27), size: 26),
                 label: 'Pedidos',
                 tooltip: '',
               ),
               NavigationDestination(
-                icon: Icon(Icons.local_shipping_outlined, size: 24),
+                icon: Icon(Icons.local_shipping_outlined, size: 26),
                 selectedIcon: Icon(Icons.local_shipping_rounded,
-                    color: Colors.indigo, size: 24),
+                    color: Color(0xFF0A1E27), size: 26),
                 label: 'Envíos',
                 tooltip: '',
               ),
